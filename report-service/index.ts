83ffee96b1c98d4989b93aea001eabccf7da5acd
Bohdan Heptinh <bheptinh@lohika.com>
2018-10-30 15:44:16 +0200

    report-service: mongodb init

--- report-service/index.ts
+++ report-service/index.ts
@@ -1,7 +1,16 @@
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
+import mongoose from 'mongoose';
+import router from './routes';
 
-import router from './routes'
+const HOST = 'localhost';
+const PORT = '16717';
+const DATABASE = 'report-service';
+
+mongoose
+    .connect(`mongodb://${HOST}:${PORT}/${DATABASE}`, { useNewUrlParser: true })
+    .then(() => console.log('Database connection successful'))
+    .catch(err => console.error(err));
 
 const app = new Koa();
 
@@ -10,4 +19,4 @@ app
     .use(router.routes())
     .use(router.allowedMethods());
 
-app.listen(3000, () => console.log("Listening port 3000"));
+app.listen(3000, () => console.log('Listening port 3000'));
