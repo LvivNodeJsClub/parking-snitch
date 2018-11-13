0526b5a2e46084c058666b34126dc0349a7ba770
Yurii Zhunkivskyi <yzhunkivskyi@lohika.com>
2018-11-13 10:39:41 +0200

    remove dist

--- image-service/dist/logger.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const winston_1 = require("winston");
-const logger = winston_1.createLogger({
-    format: winston_1.format.combine(winston_1.format.splat(), winston_1.format.simple()),
-    transports: [new winston_1.transports.Console()],
-});
-exports.default = logger;
--- image-service/dist/models/image.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const ImageSchema = new mongoose_1.Schema({
-    name: String,
-    path: String,
-});
-const Image = mongoose_1.model("Image", ImageSchema);
-exports.default = Image;
