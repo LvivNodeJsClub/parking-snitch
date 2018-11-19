71c0bbddbe080c45324c42a07017fb7d66a2f497
Yurii Zhunkivskyi <yzhunkivskyi@lohika.com>
2018-11-19 11:51:43 +0200

    promisify fs.rename

--- image-service/src/fileAdapter.ts
+++ image-service/src/fileAdapter.ts
@@ -1,6 +1,9 @@
 import fs from "fs";
+import {promisify} from "util";
 import logger from "./logger";
 import {IStorageAdapter} from "./storage";
+
+const rename = promisify(fs.rename);
 
 export default class FileAdapter implements IStorageAdapter {
     constructor(private source: string) {
@@ -9,10 +12,7 @@ export default class FileAdapter implements IStorageAdapter {
     public async save(oldPath: string, fileName: string): Promise<string> {
         const newPath = this.getNewPath(fileName);
         try {
-            // @ts-ignore
-            await fs.rename(oldPath, newPath, (e) => {
-                if (e) { throw e; }
-            });
+            await rename(oldPath, newPath);
         } catch (e) {
             logger.error("Error while moving file", oldPath, newPath, e);
             throw e;
