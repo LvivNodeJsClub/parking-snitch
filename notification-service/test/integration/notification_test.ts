441a986e74c234187b01b232f25adeea582f6415
Volodymyr Bodnar <volodya.bodnar@gmail.com>
2019-02-15 16:07:52 +0200

    Added test for db connection

--- notification-service/test/integration/notification_test.ts
+++ notification-service/test/integration/notification_test.ts
@@ -4,21 +4,27 @@ import {expect} from 'chai';
 import app from '../../src/app';
 import {Server} from "http";
 import HttpStatusCodes from 'http-status-codes';
+import { MongoClient, Collection } from 'mongodb';
 
 describe('Notification integration test', function () {
     const PORT = 3000;
+    let mongoClient: MongoClient;
+    let collection: Collection<any>;
 
     let server: Server;
 
-    before(function () {
+    before(async function () {
         server = app.listen(PORT);
+        mongoClient = await MongoClient.connect('mongodb://localhost:32772', { useNewUrlParser: true });
+        collection = await mongoClient.db('notifications_test').collection('Notifications');
     });
 
-    after(function () {
+    after(async function () {
         server.close();
+        await mongoClient.close();
     });
 
-    it('should successfully save notification', async function () {
+    it('should receive response from endpoint', async function () {
         const url = '/notify';
         const response = await request(server)
             .post(url)
@@ -29,5 +35,26 @@ describe('Notification integration test', function () {
             .expect(HttpStatusCodes.CREATED);
 
         expect(response.body.id).to.be.a('string');
+    });
+
+    it('should store notification in database', async function () {
+        const url = '/notify';
+        const inspectorId = 1234;
+        const reportId = 321;
+        const response = await request(server)
+            .post(url)
+            .send({
+                inspectorId: inspectorId,
+                reportId: reportId
+            }).set('Accept', 'application/json')
+            .expect(HttpStatusCodes.CREATED);
+
+        const resultFromDB = await collection.findOne({
+            _id: response.body.id
+        });
+
+        expect(resultFromDB).not.to.be.null;
+        expect(resultFromDB).to.haveOwnProperty('inspectorId').to.be.equal(inspectorId);
+        expect(resultFromDB).to.haveOwnProperty('reportId').to.be.equal(reportId);
     });
 });
