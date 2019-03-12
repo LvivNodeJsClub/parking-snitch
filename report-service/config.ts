ec19360095c824b70db73973d80182c3c7336450
Ihor Banadiga <banadiga@gmail.com>
2019-03-13 00:44:55 +0200

    fix for docker-compose added

--- report-service/config.ts
+++ report-service/config.ts
@@ -1,9 +1,9 @@
 
 const config = {
     reportReadyForProcessingQueue: {
-        host: process.env.QUEUE_HOST || "localhost",
-        port: process.env.QUEUE_PORT || 5672,
-        queueName: process.env.IMAGES_QUEUE_NAME || "report_ready_for_processing_queue",
+        host: process.env.REPORT_QUEUE_HOST || "localhost",
+        port: process.env.REPORT_QUEUE_PORT || 5672,
+        queueName: process.env.REPORT_QUEUE_NAME || "report_ready_for_processing_queue",
         connection: "",
     }
 };
