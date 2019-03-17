419e4c3b8b92f9bdc50135af89b4d48057b74585
Andriy Heptinh <fylhsq123@users.noreply.github.com>
2019-03-18 00:00:45 +0200

    Docker for notification service (#16)
    * Added docker for notification service

--- notification-service/src/app.ts
+++ notification-service/src/app.ts
@@ -3,12 +3,22 @@ import bodyParser from 'koa-bodyparser';
 import routes from './routes/notification';
 import mongoose from 'mongoose';
 
+const {DB_PORT = 27017, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME} = process.env;
+
 const app = new Koa();
 
 app.use(bodyParser());
 
 app.use(routes.routes());
 
-mongoose.connect(`mongodb://localhost:32768/notifications_test`, {useNewUrlParser: true});
+mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/`, {
+    useNewUrlParser: true,
+    user: DB_USER,
+    pass: DB_PASSWORD,
+    dbName: DB_NAME,
+}).catch((error) => {
+    console.error(error);
+    process.exit(1);
+});
 
 export default app;
