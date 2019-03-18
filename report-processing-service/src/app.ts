5e5e0e3581d44a0fe8bd5d63cef51858422cb319
Ihor Banadiga <banadiga@gmail.com>
2019-03-18 23:21:50 +0200

    report processing config added

--- report-processing-service/src/app.ts
+++ report-processing-service/src/app.ts
@@ -1,13 +1,7 @@
-import config from './config';
-import consumer from './queueConsumer/consumer';
-import healthcheck from './routes/healthcheck';
 import Koa from 'koa';
-import {reportReadyHandler} from './handler/reportReady';
-
+import healthcheck from './routes/healthcheck';
 
 const app = new Koa();
 app.use(healthcheck.routes());
-
-consumer(config.reportReadyForProcessingQueue, reportReadyHandler);
 
 export {app};
--- report-processing-service/src/config.ts
+++ report-processing-service/src/config.ts
@@ -1,12 +1,17 @@
 
 const config = {
+    inspectorBasePath: process.env.INSPECTOR_BASE_PATH || 'http://localhost:3003',
+    notificationBasePath: process.env.NOTIFICATION_BASE_PATH || 'http://localhost:3004',
+    reportBasePath: process.env.REPORT_BASE_PATH || 'http://localhost:3001',
     reportReadyForProcessingQueue: {
-        host: process.env.REPORT_QUEUE_HOST || "localhost",
+        connection: '',
+        host: process.env.REPORT_QUEUE_HOST || 'localhost',
         port: process.env.REPORT_QUEUE_PORT || 5672,
-        queueName: process.env.REPORT_QUEUE_NAME || "report_ready_for_processing_queue",
-        connection: "",
-    }
+        queueName: process.env.REPORT_QUEUE_NAME || 'report_ready_for_processing_queue',
+    },
 };
 
-config.reportReadyForProcessingQueue.connection = `amqp://${config.reportReadyForProcessingQueue.host}:${config.reportReadyForProcessingQueue.port}`;
+config.reportReadyForProcessingQueue.connection =
+    `amqp://${config.reportReadyForProcessingQueue.host}:${config.reportReadyForProcessingQueue.port}`;
+
 export default config;
