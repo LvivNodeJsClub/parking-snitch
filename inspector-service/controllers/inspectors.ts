c11bb904c3dafa42833a73630b45d1196f395d8c
Andrii Heptinh <aheptinh@lohika.com>
2019-03-18 17:12:53 +0200

    Added possibility to find distance locally via math.

--- inspector-service/controllers/inspectors.ts
+++ inspector-service/controllers/inspectors.ts
@@ -1,9 +1,9 @@
 import {BadRequestError, NotFoundError} from '../errorHandler/customErrors';
-import {Inspector as InspectorsModel} from '../models/inspectors';
+import {Inspector as InspectorsModel, InspectorModel} from '../models/inspectors';
 import statusCodes from 'http-status-codes';
 import {Request, Response} from "express";
 import {googleapiConfigs} from '../config';
-import {MAX_INSPECTOR_WALK_DURATION, MAX_INSPECTOR_WALK_DISTANCE} from '../constants'
+import {MAX_INSPECTOR_WALK_DURATION, MAX_INSPECTOR_WALK_DISTANCE} from '../constants';
 
 const googleMapsAPIClient = require('@google/maps').createClient({key: process.env.GOOGLE_API_KEY, Promise});
 
@@ -48,17 +48,29 @@ export async function deleteInspector(req: Request, res: Response) {
 export async function getNearest(req: Request, res: Response) {
     const inspectors = await InspectorsModel.find({}).where('deleted', false);
     if (!inspectors.length) throw new NotFoundError('No inspectors were found');
+    if (!req.query.lat || !req.query.lon) throw new BadRequestError('Destination coordinates must be specified');
+
+    let result: any;
+    if (process.env.CALC_DISTANCE_WITH_GOOGLE_API === 'TRUE') {
+        result = await findNearestViaGoogleAPI(inspectors, req.query);
+    } else {
+        result = findNearestViaMath(inspectors, req.query);
+    }
+
+    res.send(result.length ? result[0] : new NotFoundError('No nearby inspectors were found'));
+}
+
+async function findNearestViaGoogleAPI(inspectors: Array<InspectorModel>, bastard: any) {
+    console.log('Finding nearest via Google Maps API');
     const origins = inspectors.map(({location}) => `${location.lat}, ${location.lon}`);
-    const destinations = `${req.query.lat}, ${req.query.lon}`;
-    if (!destinations) throw new BadRequestError('Destination coordinates must be specified');
+    const destinations = `${bastard.lat}, ${bastard.lon}`;
     const params = {
         ...googleapiConfigs,
         origins,
         destinations
     };
     const result: any = await googleMapsAPIClient.distanceMatrix(params).asPromise();
-
-    const inspectorsByDistance = inspectors
+    return inspectors
         .map((inspector, index) => {
             const element = result.json.rows[index].elements[0];
             const distance = element.status === 'OK' ? element.distance.value : null;
@@ -67,6 +79,27 @@ export async function getNearest(req: Request, res: Response) {
         })
         .filter((inspector) => inspector.distance && (inspector.duration < MAX_INSPECTOR_WALK_DURATION || inspector.distance < MAX_INSPECTOR_WALK_DISTANCE))
         .sort((a, b) => a.distance - b.distance);
+}
 
-    res.send(inspectorsByDistance.length ? inspectorsByDistance[0] : new NotFoundError('No nearby inspectors were found'));
+function findNearestViaMath(inspectors: Array<InspectorModel>, bastard: any) {
+    console.log('Finding nearest via math');
+    return inspectors.map(inspector => {
+        return {...inspector.toJSON(), distance: getDistanceFromLatLon(inspector.location.lat, inspector.location.lon, bastard.lat, bastard.lon)}
+    }).sort((a, b) => a.distance - b.distance);
+}
+
+function getDistanceFromLatLon(lat1: number, lon1: number, lat2: number, lon2: number): number {
+    const earthRadius: number = 6371000; // Radius of the earth in m
+    const dLat: number = deg2rad(lat2 - lat1);
+    const dLon: number = deg2rad(lon2 - lon1);
+    const a: number =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const c: number = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return earthRadius * c; // Distance in m
+}
+
+function deg2rad(deg: number): number {
+    return deg * (Math.PI / 180)
 }
