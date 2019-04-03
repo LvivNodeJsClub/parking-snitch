72f032fc6f1ac58407cf42c8e401ff61fb7ff3d3
Mykola Pelekh <mpelekh@lohika.com>
2019-04-03 23:51:02 +0300

    Add unit-test for healthCheckController

---
+++ photo-service/tests/controllers/healthcheckController.test.ts
@@ -0,0 +1,15 @@
+import healthCheckController from "../../src/controllers/healthcheckController";
+
+describe("healthCheckController", () => {
+    it("should return 200 OK", () => {
+        const request: any = {};
+        const response: any = {
+            send: jest.fn(),
+        };
+
+        healthCheckController(request, response);
+
+        expect(response.send).toBeCalledTimes(1);
+        expect(response.send).toBeCalledWith(200);
+    });
+});
