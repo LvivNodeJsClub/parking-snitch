cbb052d976ce7a34604f73fee2f06808374ac163
Tymur Morozov <tmorozov@lohika.com>
2019-04-09 15:01:43 +0300

    relative path to swagger.yaml

--- report-service/src/server.ts
+++ report-service/src/server.ts
@@ -4,10 +4,11 @@ import bodyParser from "koa-bodyparser";
 import router from "./routes";
 import {loadDocumentSync} from 'swagger2';
 import { ui } from 'swagger2-koa';
+import {resolve} from "path";
 
 const app = new Koa();
 
-const document = loadDocumentSync('./swagger.yaml');
+const document = loadDocumentSync(resolve(__dirname, './swagger.yaml'));
 
 app
     .use(errorHandler)
