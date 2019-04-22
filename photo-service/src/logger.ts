b369bb3be31acb4311b67411026fd72de06302bf
Mykola Pelekh <nick.peleh@gmail.com>
2019-04-22 15:57:32 +0300

    Update logger config for photo-service (#32)

--- photo-service/src/logger.ts
+++ photo-service/src/logger.ts
@@ -1,12 +1,21 @@
-import { createLogger, format, transports } from "winston";
+import {createLogger, format, transports} from "winston";
+
+const {combine, timestamp, printf} = format;
+
+const logFormat = printf(({level, message, label, timestamp}) => {
+    return `${timestamp} ${level}: ${message}`;
+});
 
 const logger = createLogger({
-    format: format.combine(
-        format.splat(),
-        format.simple(),
-      ),
+    format: combine(
+        timestamp(),
+        logFormat
+    ),
     level: "info",
-    transports: [new transports.Console()],
+    transports: [
+        new transports.Console(),
+        new transports.File({filename: 'error.log', level: 'error'}),
+    ],
 });
 
 export default logger;
