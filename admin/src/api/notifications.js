afa6cd65ce2a9a29eda3a060047f4d58b54b87fb
Bohdan Heptinh <bheptinh@lohika.com>
2019-05-15 11:09:49 +0300

    UI Admin: Added Service worker for prod

--- admin/src/api/notifications.js
+++ admin/src/api/notifications.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const notificationsFetch = axios.create({
-    baseURL: 'http://192.168.99.100:3004/',
+    baseURL: 'http://localhost:3004/',
 });
 
 /**
--- admin/src/main.js
+++ admin/src/main.js
@@ -2,18 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-//import './registerServiceWorker'
+import './registerServiceWorker'
 
 // Material UI
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/theme/default.css'
 import 'vue-material/dist/vue-material.min.css'
+
 Vue.use(VueMaterial);
 
 Vue.config.productionip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app');
