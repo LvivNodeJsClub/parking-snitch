c331111566ba9b55639edfcb653a15e7e4d984f1
Tymur Morozov <tmorozov@lohika.com>
2019-07-01 14:05:51 +0300

    move queue k8s confing into service subdirectories

--- k8s-stop.sh
+++ k8s-stop.sh
@@ -7,7 +7,7 @@ kubectl delete -n ${NS} -f notification-service/notification-service.yml
 kubectl delete -n ${NS} -f admin-api-gateway/admin-api-gateway.yml
 kubectl delete -n ${NS} -f report-service/report-service.yml
 
-kubectl delete -n ${NS} -f photo-queue/photo-queue.yml
-kubectl delete -n ${NS} -f reports-queue/reports-queue.yml
+kubectl delete -n ${NS} -f photo-service/photo-queue.yml
+kubectl delete -n ${NS} -f report-service/reports-queue.yml
 
 kubectl delete -f parking-snitch.yml
