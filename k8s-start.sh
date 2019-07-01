792f929b5c6f796ffdc391740d5eb9ebbc42b97c
Tymur Morozov <tmorozov@lohika.com>
2019-07-01 14:23:44 +0300

    rename queues

--- k8s-start.sh
+++ k8s-start.sh
@@ -6,8 +6,8 @@ sleep 15
 
 NS=parking-snitch-production
 
-kubectl apply -n ${NS} -f photo-service/photo-queue.yml
-kubectl apply -n ${NS} -f report-service/reports-queue.yml
+kubectl apply -n ${NS} -f photo-service/photo-service-queue.yml
+kubectl apply -n ${NS} -f report-service/report-service-queue.yml
 
 kubectl apply -n ${NS} -f inspector-service/inspector-service.yml
 kubectl apply -n ${NS} -f notification-service/notification-service.yml
